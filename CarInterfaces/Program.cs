f937591cc2079f4eb552ab5843686cd38cd0e044
Hellowalls1 <thesingingbutcher@gmail.com>
2020-09-15 22:31:17 -0500

    modified functionality

--- CarInterfaces/Program.cs
+++ CarInterfaces/Program.cs
@@ -6,24 +6,40 @@ namespace CarInterfaces
     {
         static void Main(string[] args)
         {
-            Sedan s = new Sedan(60);
-            Sedan t = new Sedan(70);
-            Truck a = new Truck(45, 500);
+            Console.WriteLine("Welcome  to Car Interfaces!");
+            Console.WriteLine("Please select a sedan or truck.");
 
+            string userAnswer = Console.ReadLine();
+
+            Console.WriteLine("Please enter a speed.");
+            string userSpeed = Console.ReadLine();
+            int newSpeed = int.Parse(userSpeed);
+
+            if (userAnswer == "sedan")
+            {
+            Sedan s = new Sedan(newSpeed);
             Console.WriteLine($"The s sedan goes {s.Speed} mph and has {s.Wheels} wheels.");
 
-            Console.WriteLine($"The t sedan goes {t.Speed} mph and has {t.Wheels} wheels.");
-
+            } else
+            {
+            Truck a = new Truck(newSpeed, 500);
             Console.WriteLine($"The a truck has {a.Speed} wheels, goes {a.Wheels} mph.");
 
+            }
+           
 
-            s.SpeedUp();
-            t.SpeedUp();
-            a.SpeedUp();
 
-            Console.WriteLine($"s new speed is {s.Speed}");
-            Console.WriteLine($"a new speed is {a.Speed}");
-            Console.WriteLine($"t new speed is {t.Speed}");
+            
+
+
+
+           // s.SpeedUp();
+           // t.SpeedUp();
+          //  a.SpeedUp();
+
+          //  Console.WriteLine($"s new speed is {s.Speed}");
+          //  Console.WriteLine($"a new speed is {a.Speed}");
+          //  Console.WriteLine($"t new speed is {t.Speed}");
         }
     }
 }
