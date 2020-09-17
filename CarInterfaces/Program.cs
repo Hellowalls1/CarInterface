e76df2c6c2ae0c790603bee7f9f3e190ebaccd04
Hellowalls1 <thesingingbutcher@gmail.com>
2020-09-16 23:06:23 -0500

    new commit

--- CarInterfaces/Program.cs
+++ CarInterfaces/Program.cs
@@ -11,24 +11,50 @@ namespace CarInterfaces
 
             string userAnswer = Console.ReadLine();
 
-            Console.WriteLine("Please enter a speed.");
-            string userSpeed = Console.ReadLine();
-            int newSpeed = int.Parse(userSpeed);
-
+          //  Console.WriteLine("Please enter a speed.");
+          //  string userSpeed = Console.ReadLine();
+         //   int newSpeed = int.Parse(userSpeed);
             if (userAnswer == "sedan")
             {
-            Sedan s = new Sedan(newSpeed);
-            Console.WriteLine($"The s sedan goes {s.Speed} mph and has {s.Wheels} wheels.");
+                Console.WriteLine("Please enter a speed.");
+                string userSpeed = Console.ReadLine();
+                int newSpeed = int.Parse(userSpeed);
 
-            } else
+                Sedan s = new Sedan(newSpeed);
+                Console.WriteLine($"The s sedan goes {s.Speed} mph and has {s.Wheels} wheels.");
+
+            } else if (userAnswer == "truck")
             {
-            Truck a = new Truck(newSpeed, 500);
-            Console.WriteLine($"The a truck has {a.Speed} wheels, goes {a.Wheels} mph.");
+                Console.WriteLine("How much does your truck weigh? Enter a number between 1-900000.");
+                string weight = Console.ReadLine();
+                int truckWeight = int.Parse(weight);
+
+                Console.WriteLine("Please enter a speed.");
+                string userSpeed = Console.ReadLine();
+                int newSpeed = int.Parse(userSpeed);
+
+                Truck a = new Truck(newSpeed, truckWeight);
+                Console.WriteLine($"Your truck has {a.Wheels} wheels, goes {a.Speed} mph.");
+
+                Console.WriteLine("You see a vehicle driving slow in the left lane");
+                Console.WriteLine("Do you want to slow down or speed up?");
+                Console.WriteLine("Please enter one or the other....");
+                string userDirection = Console.ReadLine();
+                if (userDirection == "slow down")
+                {
+                    a.SlowDown();
+                    Console.WriteLine($"Your new speed is {a.Speed}");
+                } 
+
+
 
             }
-           
+            else
+            {
+                Console.WriteLine("That's not what the application asked. Goodbye!");
+            }
 
-
+       
             
 
 
